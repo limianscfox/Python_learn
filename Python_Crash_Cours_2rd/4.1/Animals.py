852d13888af20d23e9881a3e7ec3f1c6e98f33e2
limianscfox <limian489@gmail.com>
2023-06-16 15:22:38 +0800

    update 4.2

---
+++ Python_Crash_Cours_2rd/4.1/Animals.py
@@ -0,0 +1,4 @@
+animals = ['dog','cat','cow']
+for animal in animals:
+    print(f"A {animal} would make a great pet.")
+print(f"Any of these animals would make a great pet!")
---
+++ Python_Crash_Cours_2rd/4.1/pizza.py
@@ -0,0 +1,4 @@
+pizzas = ['1','2','3','4']
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+print(f"I really love pizza!")
